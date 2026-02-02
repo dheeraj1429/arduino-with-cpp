d176be5d8e71c4a3640e145ffa5f64b96301f6bd
Dee <86516056+dheeraj1429@users.noreply.github.com>
2026-02-02 17:36:11 +0530

    learing

--- Learning/src/analogJoystick.cpp
+++ Learning/src/analogJoystick.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <Servo.h>
 
 #define VERT_PIN A0
 #define HORZ_PIN A1
@@ -8,12 +9,22 @@ int xValue;
 int yValue;
 int buttonState;
 
+const int servoPin = 11;
+double servoPos = 0.0;
+double servoStep = 1.0;
+
+Servo arm;
+
 void setup() {
     Serial.begin(9600);
 
     pinMode(VERT_PIN, INPUT);
     pinMode(HORZ_PIN, INPUT);
     pinMode(SEL_PIN, INPUT_PULLUP);
+
+    // setup servo
+    arm.attach(servoPin);
+    arm.write(servoPos);
 }
 
 void loop() {
@@ -21,12 +32,14 @@ void loop() {
     int hort = analogRead(HORZ_PIN);
     bool selPressed = digitalRead(SEL_PIN) == LOW;
 
-    Serial.print("VERT: ");
-    Serial.print(vert);
-    Serial.print(" | HORT : ");
-    Serial.print(hort);
-    Serial.print(" | Pressed : ");
-    Serial.println(selPressed);
+    // converting the hort value to from 0 - 1023 to 0 - 180;
+    int rotation = map(hort, 0, 1023, 0, 180);
+    Serial.print("Rotation: ");
+    Serial.print(rotation);
+    Serial.print(" | Vertical: ");
+    Serial.println(vert);
 
-    delay(1000);
+    arm.write(rotation);
+
+    // delay(1000);
 }
